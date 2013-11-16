98f203b294a4c8eb46a51f4338ac9fcb2691dbd4
vishpat <vishpat@gmail.com>
2013-11-16 18:32:03 -0500

    Checkpoint

--- python/graph/escape.py
+++ python/graph/escape.py
@@ -2,12 +2,12 @@
 
 class Escape:
     
-    def lowest(self, deadly_areas, harmful_areas):
+    def lowest(self, harmful_areas, deadly_areas):
         
         safe = 0
         harmful = 1
         deadly = 2
-        size = 500 
+        size = 5 
 
         matrix = list()
         for row in range(0, size):
@@ -19,18 +19,17 @@ class Escape:
         
         for h in harmful_areas:
             x1, y1, x2, y2 = [int(n) for n in h.split()]
-            for i in range(x1, x2 + 1):
-                for j in range(y1, y2 + 1):
+            for i in range(min(x1, x2), max(x1, x2)):
+                for j in range(min(y1, y2), max(y1, y2)):
                     matrix[i][j] = harmful
 
         for d in deadly_areas:
             x1, y1, x2, y2 = [int(n) for n in d.split()]
-            for i in range(x1, x2 + 1):
-                for j in range(y1, y2 + 1):
+            for i in range(min(x1, x2), max(x1, x2)):
+                for j in range(min(y1, y2), max(y1, y2)):
                     matrix[i][j] = deadly 
 
-        print str(matrix)
-       
+        
 if __name__ == "__main__":
     esc = Escape()
-    esc.lowest(["500 0 0 500"], ["0 0 0 0"])
+    esc.lowest(["5 0 0 5"], ["0 0 0 0"])
