84e314ae7646a4d11f1a761d6971e0e25af6d095
vishpat <vishpat@gmail.com>
2013-11-30 15:24:35 -0500

    Completed emitting the gcode

--- python/svg/svg2gcode.py
+++ python/svg/svg2gcode.py
@@ -3,23 +3,28 @@
 from xml.dom import minidom
 import sys
 
-import shapes
+import shapes as shapes_pkg
+from shapes import point_generator
 
 svg_shapes = ['rect', 'circle', 'ellipse', 'line', 'polyline', 'polygon', 'path']
 
-def parse_svg():
+def generate_gcode():
     dom = minidom.parse(sys.stdin)
-    
+    print "G28\n\n G1 Z5.0\n\n" 
     for svg_shape in svg_shapes:
-        paths = dom.getElementsByTagName(svg_shape)
-        for path in paths:
-            shape_class = getattr(shapes, path.nodeName)
-            shape_obj = shape_class(path.toxml())
-            print shape_obj.svg_path()
+        shapes = dom.getElementsByTagName(svg_shape)
+        for shape in shapes:
+            shape_class = getattr(shapes_pkg, shape.nodeName)
+            shape_obj = shape_class(shape.toxml())
+            print "G4 P200"
+            p = point_generator(shape_obj.d_path())
+            for x,y in p:
+                print "G1 X%0.1f Y%0.1f" % (x, y) 
+    print "G28"
     dom.unlink()
 
 if __name__ == "__main__":
-    parse_svg()
+    generate_gcode()
 
 
 
