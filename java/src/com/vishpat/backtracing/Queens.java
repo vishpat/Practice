70a51532d15958676b740970d592fef453663b95
Vishal Patil <vishpat@gmail.com>
2013-12-02 16:57:38 -0500

    checkpoint

--- java/src/com/vishpat/backtracing/Queens.java
+++ java/src/com/vishpat/backtracing/Queens.java
@@ -1,4 +1,43 @@
 package com.vishpat.backtracing;
+import java.util.Vector;
+
+class Pos {
+    public int x;
+    public int y;
+}
+
+class Node {
+    private Vector<Pos> positions;
+    private Vector<Node> children;
+    private int board_size;
+
+    public Node(Vector<Pos> positions, int board_size) {
+        this.positions = positions;
+        this.board_size = board_size;
+    }
+    
+    public boolean isLeaf() {
+        return this.board_size == this.positions.size();
+    }
+
+    public Vector<Pos> getValidPositions(Pos pos) {
+        
+        Vector<Pos> validPositions = new Vector<Pos>();
+        int xpos = 0;
+        int ypos = 0;
+        
+        for (int i = 0; i < this.board_size; i++) {
+            for (int j = 0; j < this.board_size; j++) {
+                
+                if (i == pos.x || j == pos.y) {
+                    continue;
+                }
+            }
+        }    
+        
+        return validPositions;
+    }
+}
 
 class Queens {
     public static void main(String[] args) {
