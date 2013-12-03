a1977012e396a6388b78bd52e72cbb301b9d606b
Vishal Patil <vishpat@gmail.com>
2013-12-03 15:58:24 -0500

    Finished bridge crossing

--- java/src/com/vishpat/backtracing/BridgeCrossing.java
+++ java/src/com/vishpat/backtracing/BridgeCrossing.java
@@ -1,7 +1,10 @@
+// Solved SRM 146 Div 2 (3rd problem)
+
 package com.vishpat.backtracing;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 enum flashLightPos {
     LEFT, RIGHT;
@@ -33,7 +36,20 @@ class Node {
             Integer left[] = this.left.toArray(new Integer[this.left.size()]);
             for (int i = 0; i < left.length; i++) {
                 for (int j = i + 1; j < left.length; j++) {
+                    Set<Integer> childLeft = new HashSet<Integer>();
+                    childLeft.addAll(this.left);
+                    childLeft.remove(left[i]);
+                    childLeft.remove(left[j]);
 
+                    Set<Integer> childRight = new HashSet<Integer>();
+                    childRight.addAll(this.right);
+                    childRight.add(left[i]);
+                    childRight.add(left[j]);
+     
+                    int childCost = this.cost + Math.max(left[i], left[j]);
+                    Node childNode = new Node(this.times, childLeft, childRight, 
+                                flashLightPos.RIGHT, childCost);
+                    children.add(childNode);
                 }
             }
         } else {
@@ -43,7 +59,7 @@ class Node {
                 childLeft.add(i);
                 
                 Set<Integer> childRight = new HashSet<Integer>();
-                childRight.addAll(this.left);
+                childRight.addAll(this.right);
                 childRight.remove(i);
  
                 int childCost = this.cost + i;
@@ -60,18 +76,61 @@ class Node {
         return right.size() == times.length;
     }
 
+    public void printConfiguration() {
+
+        System.out.format("Flashlight %s: Left : {", 
+                    this.pos == flashLightPos.LEFT ? "LEFT": "RIGHT");
+        
+        for (int i : this.left) {
+            System.out.format("%d,", i);
+        }
+
+        System.out.print("} Right: {");
+        for (int i : this.right) {
+            System.out.format("%d, ", i);
+        }
+
+        System.out.print("}\n");
+    }
 }
 
 
 class BridgeCrossing {
     public static void main(String[] args) {
-        int times[] = {1, 2, 5, 10, 20};
-        
-        for (int i = 0; i < times.length; i++) {
-            for (int j = i + 1; j < times.length; j++) {
-                System.out.format("(%d, %d) ", times[i], times[j]);
-            }
+        int times[] = {1, 2, 3, 50, 99, 100};
+        Stack<Node> stack = new Stack<Node>();    
+        Set<Integer> left = new HashSet<Integer>();
+        int minCost = Integer.MAX_VALUE;
+
+        if (times.length == 1) {
+            System.out.format("%d\n", times[0]);
+            return;
         }
 
+        for (int i : times) {
+            left.add(i);
+        }
+
+        Set<Integer> right = new HashSet<Integer>();
+        
+        Node n = new Node(times, left, right, flashLightPos.LEFT, 0);
+        stack.push(n);
+        
+        while (!stack.empty()) {
+            Node childNode = stack.pop();
+            if (childNode.isLeaf()) {
+                
+                if (childNode.cost < minCost) {
+                    minCost = childNode.cost;
+                }
+
+            } else {
+                for (Node cn : childNode.getChildren()) {
+                    stack.push(cn);
+                }
+            }
+        }
+        
+        System.out.format("%d\n", minCost);
     }
 }
