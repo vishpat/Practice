74a89485e76fb7ae84ed341e00af0625f0f5fc89
Vishal Patil <vishpat@gmail.com>
2013-12-05 22:15:13 -0500

    string permutations complete

--- c/test/src/vp_string.c
+++ c/test/src/vp_string.c
@@ -12,21 +12,16 @@ swap(char *x, char *y)
 void
 vp_string_permutations(char *str, int start_idx, int end_idx)
 {
-    int i = start_idx;
-
     if (start_idx == end_idx) {
         printf("%s\n", str);
         return;
     }
-
-    while (i <= end_idx ) {
-           swap(&str[i], &str[start_idx]);
-           if (i != start_idx) {
-                printf("%s\n",  str);
-           }
-           vp_string_permutations(str, start_idx + 1, end_idx);
-           swap(&str[i], &str[start_idx]);
-           i++;
+    
+    int i = start_idx;
+    while (i <= end_idx) {
+        swap(&str[i], &str[start_idx]);
+        vp_string_permutations(str, start_idx + 1, end_idx);
+        swap(&str[i], &str[start_idx]);
+        i++;
     }
-
 }
