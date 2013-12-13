23cdd33ec4a7925d16b403d5c4edcc4d66b46ce4
Vishal Patil <vishpat@gmail.com>
2013-12-13 04:29:12 -0500

    Completed coin change

--- java/src/com/vishpat/dp/Change.java
+++ java/src/com/vishpat/dp/Change.java
@@ -5,24 +5,37 @@ import java.util.ArrayList;
 
 class CoinChange {
    
-    private final int[] coins = {1, 5, 10, 21, 25}; 
+    private final int[] coins = {1,5,10,21,25}; 
 
-    public CoinChange()
+    public void solve()  
     {
-        Arrays.sort(coins);
-    }
-
-    public ArrayList<int> solve(int change)
-    {
-        int i = 0;
-
-        for (c : this.coins) {
-            minCoins[i].add(c);
-            minCoins[i].addAll(solve(change - c);
+        int sum = 63;
+        int[] sum_arr = new int[sum + 1];
+       
+        for (int i = 0; i < sum + 1 ; i++) {
+            sum_arr[i] = Integer.MAX_VALUE; 
         }
+        
+        sum_arr[0] = 0;
+        for (int i = 1; i < sum + 1; i++) {
+            
+            for (int c : coins) {
+                if (i == c) {
+                    sum_arr[i] = 1;
+                    break;
+                }
+
+                if (i >= c &&  sum_arr[i] > sum_arr[i - c] + 1) {
+                    sum_arr[i] = sum_arr[i - c] + 1;
+                }
+            }
+       }
+
+       System.out.format("%d\n", sum_arr[sum]);
     }
 
     public static void main(String[] args) {
-
+        CoinChange c = new CoinChange();
+        c.solve();
     }
 }
