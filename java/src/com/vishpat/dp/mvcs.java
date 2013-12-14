061a38933096c2a5e4e1b51761b8a70b2c1f7594
Vishal Patil <vishpat@gmail.com>
2013-12-14 14:59:18 -0800

    mvcs complete

--- java/src/com/vishpat/dp/mvcs.java
+++ java/src/com/vishpat/dp/mvcs.java
@@ -3,10 +3,27 @@ package com.vishpat.dp;
 public class mvcs {
 
     static void solve() {
+        int sequence[] = {-15, 29, -36, 3, -22, 11, 19, -5};
+        int sum[] = new int[sequence.length];
+        int max_sum = 0;
+        sum[0] = sequence[0];
+        for (int i = 1; i < sequence.length; i++) {
+            
+            if (sum[i - 1] + sequence[i] > sequence[i]) {
+                sum[i] = sum[i - 1] + sequence[i];
+            } else {
+                sum[i] = sequence[i];
+            }
+            
+            if (sum[i] > max_sum) {
+                max_sum = sum[i];
+            }
+        }
 
+        System.out.format("%d\n", max_sum);
     }
 
     public static void main(String[] args) {
-
+        solve();
     }
 }
