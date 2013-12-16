9f27208fda704858e4439fc7ac872a1a241f27de
Vishal Patil <vishpat@gmail.com>
2013-12-15 19:48:50 -0500

    Checkpoint for longest increasing subsequence

--- java/src/com/vishpat/dp/LIS.java
+++ java/src/com/vishpat/dp/LIS.java
@@ -2,27 +2,30 @@ package com.vishpat.dp;
 
 public class LIS {
     
-    static int solve() {
+    static void solve() {
         int sequence[] = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
-        int length_arr[] = new int[sequence.length];
+        int seq_len_arr[] = new int[sequence.length];
         int parent_arr[] = new int[sequence.length];
         int max_idx = 0;
         int max_seq_len = 0;
-        
-        length_arr[0] = 0;
+       
+        for (int i = 0; i < sequence.length; i++) {
+            seq_len_arr[i] = 1;
+        }
+
         for (int i = 1; i < sequence.length; i++) {
-            if (sequence[i] > sequence[i - 1]) {
-                length_arr[i] = length_arr[i - 1] + 1;
-                parent_arr[i] = i - 1;
-                if (max_seq_len < length_arr[i]) {
-                    max_seq_len = length_arr[i];
-                    max_idx = i;
+            for (int j = 0; j < i; j++) {
+                if (sequence[i] > sequence[j]) {
+                    seq_len_arr[i] += 1;
+
+                    if (max_seq_len < seq_len_arr[i]) {
+                        max_seq_len = seq_len_arr[i];
+                    }
                 }
-            } else {
-                length_arr[i] = length_arr[i - 1];
-                parent_arr[i] = parent_arr[i - 1];
             }
         }
+
+        System.out.format("%d\n", max_seq_len);
     }
 
     public static void main(String[] args) {
