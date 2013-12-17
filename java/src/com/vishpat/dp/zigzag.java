2306b655305ad2ef51ec1b9afa38d8e2629a2de8
Vishal Patil <vishpat@gmail.com>
2013-12-17 16:21:51 -0500

    Zigzag checkpoint

--- java/src/com/vishpat/dp/zigzag.java
+++ java/src/com/vishpat/dp/zigzag.java
@@ -1,13 +1,27 @@
 package com.vishpat.dp;
 
+enum sign {
+    positive,
+    negative
+};
+
 class zigzag {
 
     public static int longestZigZag(int[] sequence) {
-        int dp[] = new int[sequence.length];
 
-        for (int i = 1; i < sequence.length; i++) {
-
+        if (sequence.length < 2) {
+            return 1;
         }
+
+        sign current_sign = (sequence[1] > sequence[0]) ? 
+                            sign.positive : sign.negative;
+
+        int dp[] = new int[sequence.length];
+        for (int i = 1; i < sequence.length; i++) {
+        
+        }
+
+        return 0;
     }
 
     public static void main(String[] args) 
