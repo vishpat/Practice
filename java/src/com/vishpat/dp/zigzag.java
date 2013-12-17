1ba4748d10d4e70bfca1132b520f409e07ac18d7
Vishal Patil <vishpat@gmail.com>
2013-12-17 17:05:47 -0500

    Zigzag solved

--- java/src/com/vishpat/dp/zigzag.java
+++ java/src/com/vishpat/dp/zigzag.java
@@ -1,10 +1,5 @@
 package com.vishpat.dp;
 
-enum sign {
-    positive,
-    negative
-};
-
 class zigzag {
 
     public static int longestZigZag(int[] sequence) {
@@ -14,6 +9,10 @@ class zigzag {
         }
 
         int dp[] = new int[sequence.length];
+        
+        dp[0] = 1;
+        dp[1] = 2;
+
         for (int i = 2; i < sequence.length; i++) {
             for (int j = i - 1; j > 0; j--) {
                 if (((sequence[i] > sequence[j] &&
@@ -35,7 +34,11 @@ class zigzag {
 
     public static void main(String[] args) 
     {
-        int[] sequence = {1,7,4,9,2,5};
+        int[] sequence = {374, 40, 854, 203, 203, 156, 362, 279, 812, 955, 
+        600, 947, 978, 46, 100, 953, 670, 862, 568, 188, 
+        67, 669, 810, 704, 52, 861, 49, 640, 370, 908, 
+        477, 245, 413, 109, 659, 401, 483, 308, 609, 120, 
+        249, 22, 176, 279, 23, 22, 617, 462, 459, 244};
         System.out.format("%d\n", longestZigZag(sequence));
     }
 }
