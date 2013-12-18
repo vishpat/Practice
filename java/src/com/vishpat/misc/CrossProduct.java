996fb04e5c79c833e0b0a2abc924306edc035318
Vishal Patil <vishpat@gmail.com>
2013-12-17 17:11:46 -0800

    Added a template for string cross product

---
+++ java/src/com/vishpat/misc/CrossProduct.java
@@ -0,0 +1,14 @@
+package com.vishpat.misc;
+
+class CrossProduct {
+    
+    static void printCrossProduct(String[] args)
+    {
+        
+    }
+
+    public static void main(String[] args) 
+    {
+        printCrossProduct(args);           
+    }
+}
