39221eb0df55403e733f543056ac2f7df099981e
Vishal Patil <vishpat@gmail.com>
2014-01-01 12:42:20 -0500

    Added template for tccc 04 round 04

---
+++ java/src/com/vishpat/dp/badneighbours.java
@@ -0,0 +1,14 @@
+package com.vishpat.dp;
+
+class BadNeighbours {
+
+    public static int getMaxDonations(int[] donations)
+    {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+       int donations[] = {10, 3, 2, 5, 7, 8}; 
+       System.out.format("%d\n", getMaxDonations(donations));
+    }
+}
