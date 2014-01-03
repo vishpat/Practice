1a5eeeaabe750f09a345bc3509d9236ea29a2721
Vishal Patil <vishpat@gmail.com>
2014-01-02 21:32:10 -0500

    Partition problem complete

--- java/src/com/vishpat/dp/BalancedParition.java
+++ java/src/com/vishpat/dp/BalancedParition.java
@@ -3,8 +3,19 @@ package com.vishpat.dp;
 import java.util.ArrayList;
 
 class BalancedParition {
-    
-    static int sum(ArrayList<int> elements, int element)
+   
+    static void printSet(String setName, ArrayList<Integer> elements)
+    {
+        System.out.format("%s: ", setName);
+
+        for (int v: elements) {
+            System.out.format("%d ", v);
+        }
+
+        System.out.format("\n");
+    }
+
+    static int sum(ArrayList<Integer> elements, int element)
     {
         int total = 0;
         for (int v : elements) {
@@ -16,8 +27,8 @@ class BalancedParition {
 
     static void solve(int[] values) 
     {
-        Arraylist s1 = new ArrayList<int>();
-        Arraylist s2 = new ArrayList<int>();
+        ArrayList <Integer>s1 = new ArrayList<Integer>();
+        ArrayList <Integer>s2 = new ArrayList<Integer>();
         
         s1.add(values[0]);
         s2.add(values[1]);
@@ -30,13 +41,21 @@ class BalancedParition {
             
             int sum2 = sum(s2, element);
             int s2_sum = sum(s2, 0);
-            
+
+            if ((sum1 - s2_sum) < (sum2 - s1_sum)) {
+                s1.add(element);
+            } else {
+                s2.add(element);
+            }
         }
+        
+        printSet("S1", s1);
+        printSet("S2", s2);
     }
 
     public static void main(String[] args) 
     {
-        int values[] = {};
+        int values[] = {2, 10, 3, 8, 5, 7, 9, 5, 3, 2};
         solve(values);
     }
 }
