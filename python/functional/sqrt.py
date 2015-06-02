f677640d8ed7f1aa2b732394eeff3d9cb23f21e8
vpati011 <vishal.patil@nuagenetworks.net>
2015-06-02 00:54:35 -0400

    Implemented sqrt function using functional programming

---
+++ python/functional/sqrt.py
@@ -0,0 +1,21 @@
+__author__ = 'vpati011'
+
+
+def repeat(a0, n):
+    while True:
+        a1 = (1.0*a0 + n/a0)/2
+        yield a1
+        a0 = a1
+
+def within(eps, a0, n):
+    a1 = n.next()
+    return a0 if abs(a0 - a1) <= eps else within(eps, a1, n)
+
+def functional_sqrt(n):
+    eps = 0.000000001
+    a0 = n/2
+    r = repeat(a0, n)
+    return within(eps, a0, r)
+
+if __name__ == "__main__":
+    print functional_sqrt(12100000000000000)
