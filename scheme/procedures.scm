b05f2d5712994f04654e751497211b168ae5aaab
vpati011 <vishal.patil@nuagenetworks.net>
2015-07-02 11:07:38 -0400

    Added procedure to calculate the square root

--- scheme/procedures.scm
+++ scheme/procedures.scm
@@ -10,3 +10,17 @@
                                     (* (min-three x y z) (min-three x y z))
                                  )
 )
+
+; 1.1.7
+(define (abs x) ( if (< x 0) (* -1 x) x))
+(define (square x) (* x x))
+(define (average x y) (/ (+ x y) 2));
+(define (improve-guess guess x) (average guess (/ x guess)))
+(define (good-enough guess x) (< (abs (- (square guess) x)) 0.001))
+(define (sqrt-iter guess x) 
+            (if (good-enough guess x) 
+                guess
+                (sqrt-iter (improve-guess guess x) x)
+            )
+)
+(define (sqrt x) (sqrt-iter 1.0 x))
