9592db60bd84ef907da5afab1de5c5c476e72267
vpati011 <vishal.patil@nuagenetworks.net>
2015-07-02 22:38:39 -0400

    Added code to find the cube root of a number

--- scheme/procedures.scm
+++ scheme/procedures.scm
@@ -17,10 +17,25 @@
 (define (average x y) (/ (+ x y) 2));
 (define (improve-guess guess x) (average guess (/ x guess)))
 (define (good-enough guess x) (< (abs (- (square guess) x)) 0.001))
+(define (good-enough-2 guess x) (< (abs (- (improve-guess guess x) guess)) 0.001))
 (define (sqrt-iter guess x) 
-            (if (good-enough guess x) 
+            (if (good-enough-2 guess x) 
                 guess
                 (sqrt-iter (improve-guess guess x) x)
             )
 )
 (define (sqrt x) (sqrt-iter 1.0 x))
+
+; 1.8
+(define (cube x) (* x x x))
+(define (good-enough-cube-root guess x) (< (abs (- (cube guess) x)) 0.001))
+(define (improve-cube-root-guess guess x) (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+(define (cube-root-iter guess x) 
+            (if (good-enough-cube-root guess x) 
+                guess
+                (cube-root-iter (improve-cube-root-guess guess x) x)
+            )
+)
+(define (cube-root x) (cube-root-iter 1.0 x))
+
+
