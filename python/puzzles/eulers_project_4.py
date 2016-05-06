21c1aecce276e87a0be8313524248e881e3b930b
Vishal Patil <vishal.patil@nuagenetworks.net>
2016-05-06 00:33:43 -0400

    Solution to euler's problem number 4

--- python/puzzles/eulers_project_4.py
+++ python/puzzles/eulers_project_4.py
@@ -7,10 +7,12 @@ def is_palindrome(num):
 
 if __name__ == "__main__":
     max_palindrome = 0
+    min_y = 100
     for x in reversed(range(100, 1000)):
         y = 999
-        while y >= x:
+        while y >= min_y:
             if is_palindrome(x*y) is True and x*y > max_palindrome:
+                min_y = y
                 max_palindrome = x*y
             y -= 1
     print max_palindrome
