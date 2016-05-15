f4cd66dc74292d233f647de3a17aaf0a384ca72b
Vishal Patil <vishal.patil@nuagenetworks.net>
2016-05-15 13:37:07 -0400

    Optimized problem 9 solution

--- python/puzzles/project_euler/problem9.py
+++ python/puzzles/project_euler/problem9.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-import sys
+import math
 
 if __name__ == "__main__":
-    for x in range(1, 1001):
-        for y in range(1, 1001 - x):
-            for z in range(1, 1001 - (x + y)):
-                    if x*x + y*y == z*z and (x + y + z == 1000):
-                        print x, y, z
-                        sys.exit(1)
+    k = int(math.sqrt(1000))
+    for x in range(1, k):
+        for y in range(1, x):
+            a = x*x - y*y
+            b = 2*x*y
+            c = x*x + y*y
+            if (c*c == (a*a + b*b)) and (a + b + c == 1000):
+                print a, b, c
