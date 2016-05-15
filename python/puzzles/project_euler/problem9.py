98bd57471ece77843425d7b1ac1f24af78a4b8b9
Vishal Patil <vishal.patil@nuagenetworks.net>
2016-05-15 10:39:21 -0400

    Solution to problem 9

---
+++ python/puzzles/project_euler/problem9.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    for x in range(1, 1001):
+        for y in range(1, 1001 - x):
+            for z in range(1, 1001 - (x + y)):
+                    if x*x + y*y == z*z and (x + y + z == 1000):
+                        print x, y, z
+                        break
