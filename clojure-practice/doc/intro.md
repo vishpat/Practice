a6025b22ed9aa09d8765c2de0c8350b929f535e2
vpati011 <vishal.patil@nuagenetworks.net>
2016-09-13 20:26:47 -0400

    Clojure Practice

---
+++ clojure-practice/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-practice
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure-practice/test/clojure_practice/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-practice.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-practice.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
