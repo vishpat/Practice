b7d490a5a7f58d5eae8c54848e72a047e3076580
vpati011 <vishal.patil@nuagenetworks.net>
2016-09-25 23:58:14 -0400

    Checkpoint

--- project-euler/src/project_euler/core.clj
+++ project-euler/src/project_euler/core.clj
@@ -1,17 +1,35 @@
 (ns project-euler.core)
 
-(defn square-numbers
-  [[& numbers]]
-  (loop [[n0 & nums] numbers squares []]
-    (if n0
-      (do
-          (def n2 (* n0 n0))
-          (recur nums (conj squares n2)))
-      squares
-      )))
+(defn sub-str
+  [m n]
+  ()
+  )
 
+(defn add-str
+    [m n]
+  (
+
+   )
+)
+
+
+(defn multiply-str
+  [m n]
+  (loop [num1 m num2 n result ""]
+    (cond (= num1 "0") "0" (= num2 "0") "0" (= num1 "1") (add-str result num2) 
+          :else (recur (sub-str num1 "1") num2 (add-str result num2)))  
+  )
+)
+
+(defn factorial
+  [n]
+  (
+   loop [x n result 1] 
+   (if (= x 1) result ( recur (- x 1) (* x result))
+   )
+  )
+)
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (square-numbers [1 2 3 4])))
-
+  (println (multiply-str 10 9)))
