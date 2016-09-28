7c9cbcf82986d39decbd97047d45cebf93ec75b8
vpati011 <vishal.patil@nuagenetworks.net>
2016-09-28 00:25:00 -0400

    Checkpoint

--- project-euler/src/project_euler/problem13.clj
+++ project-euler/src/project_euler/problem13.clj
@@ -4,6 +4,6 @@
 (defn sum-column 
   [data-file column]
   (with-open [rdr (io/reader data-file)]
-    (reduce (fn [total line] (reduce + total (map #(Integer/parseInt %) (map str (vec (seq line)))))) 0 (line-seq rdr))
+    (reduce (fn [total line] (reduce + total (map #(Integer/parseInt %) (list (str (nth (seq line) column)))))) 0 (line-seq rdr))
   )
  ) 
