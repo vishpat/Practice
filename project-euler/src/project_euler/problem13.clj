f6ffa070d89a378d7ab6c6bd593dc268b55aaac4
vpati011 <vishal.patil@nuagenetworks.net>
2016-09-28 21:57:23 -0400

    Completed problem 13

--- project-euler/src/project_euler/problem13.clj
+++ project-euler/src/project_euler/problem13.clj
@@ -6,4 +6,15 @@
   (with-open [rdr (io/reader data-file)]
     (reduce (fn [total line] (reduce + total (map #(Integer/parseInt %) (list (str (nth (seq line) column)))))) 0 (line-seq rdr))
   )
- ) 
+ )
+
+(defn solve 
+  [data-file]
+  (loop [carry 0 column 49 digits ""]
+    (if (< column 0) (str (str carry) digits) 
+      (let [column-total (+ (sum-column data-file column) (mod carry 10)) next-carry (+ (quot carry 10) (quot column-total 10))]
+          (recur next-carry (- column 1) (str (str (mod column-total 10)) digits)) 
+      ) 
+    )
+  )
+)
