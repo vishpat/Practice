27b9f7a2677cc4430c6c66d8a03158178ea3f825
vpati011 <vishal.patil@nuagenetworks.net>
2016-09-30 19:39:30 -0400

    Checkpoint

--- project-euler/src/project_euler/problem15.clj
+++ project-euler/src/project_euler/problem15.clj
@@ -1,6 +1,6 @@
 (ns project-euler.problem15)
 
-(def size 20)
+(def size 2)
 (def weight-matrix {'(0 0) 0})
 
 (defn init-map
@@ -16,11 +16,11 @@
    (init-map)
    (doseq [x (take size (range)) y (take size (range))]
        (let [my-weight (get weight-matrix (list x y)) next-x (inc x) next-y (inc y) ]
-         (println (str (str (get weight-matrix '(next-x y))) (str (get weight-matrix (list x next-y)))))
           (cond 
             (< next-x size) (def weight-matrix (update weight-matrix (list next-x y) inc)) 
             (< next-y size) (def weight-matrix (update weight-matrix (list x next-y) inc)) 
           )
+         (println weight-matrix)
          )
        )
      )
