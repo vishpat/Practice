d8e420aa7f27811632fbd27a4216823a505f8d24
vpati011 <vishal.patil@nuagenetworks.net>
2016-10-01 14:01:07 -0400

    Use atoms for maps

--- project-euler/src/project_euler/problem15.clj
+++ project-euler/src/project_euler/problem15.clj
@@ -1,14 +1,14 @@
 (ns project-euler.problem15)
 
 (def size 21)
-(def weight-matrix {'(0 0) 1})
+(def weight-matrix (atom {'(0 0) 1}))
 
 (defn init-map
   []
   (doseq [x (take size (range)) y (take size (range))]
-    (def weight-matrix (assoc weight-matrix (list x y) 0))
+    (swap! weight-matrix assoc (list x y) 0)
    )
-  (def weight-matrix (assoc weight-matrix (list 0 0) 1))
+  (swap! weight-matrix assoc (list 0 0) 1)
 )
 
 (defn solve
@@ -16,11 +16,11 @@
   (do
    (init-map)
    (doseq [x (take size (range)) y (take size (range))]
-       (let [my-weight (get weight-matrix (list x y)) next-x (inc x) next-y (inc y) ]
-          (when (< next-x size) (def weight-matrix (assoc weight-matrix (list next-x y) (+ my-weight (get weight-matrix (list next-x y))))))
-          (when (< next-y size) (def weight-matrix (assoc weight-matrix (list x next-y) (+ my-weight (get weight-matrix (list x next-y))))))
+       (let [my-weight (get @weight-matrix (list x y)) next-x (inc x) next-y (inc y) ]
+          (when (< next-x size) (swap! weight-matrix assoc (list next-x y) (+ my-weight (get @weight-matrix (list next-x y)))))
+          (when (< next-y size) (swap! weight-matrix assoc (list x next-y) (+ my-weight (get @weight-matrix (list x next-y)))))
          )
        )
      )
-  (get weight-matrix (list (- size 1) (- size 1)))
+  (get @weight-matrix (list (- size 1) (- size 1)))
   )
