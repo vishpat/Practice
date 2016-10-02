b4f71e001075d7bf5dcedac767ce5f759dbbd8e9
vpati011 <vishal.patil@nuagenetworks.net>
2016-10-02 13:30:48 -0400

    Checkpoint

--- project-euler/src/project_euler/problem12.clj
+++ project-euler/src/project_euler/problem12.clj
@@ -1,6 +1,18 @@
-(ns project-euler.problem12)
+(ns project-euler.problem12
+  (require [project-euler.util :as util]))
 
 (defn triangle-number
   [n]
   (reduce + n (range n))
+  )
+
+(defn solve
+  []
+  (loop [num 1]
+   (let [triangle-num (triangle-number num) divisors (util/get-all-factors triangle-num) divisor-count (count divisors)]
+     (println "num " num " triangle num " triangle-num " divisors " divisors " divisor-count " divisor-count) 
+     (if (= divisor-count 500) num
+       (recur (+ num 1)))
+   )
+  )
   )
