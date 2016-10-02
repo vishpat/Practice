8279a0fddd77028d75013e8e3dcfe45b0b954bf4
vpati011 <vishal.patil@nuagenetworks.net>
2016-10-02 15:35:10 -0400

    Checkpoint

--- project-euler/src/project_euler/problem12.clj
+++ project-euler/src/project_euler/problem12.clj
@@ -10,8 +10,7 @@
   []
   (loop [num 1]
    (let [triangle-num (triangle-number num) divisors (util/get-all-factors triangle-num) divisor-count (count divisors)]
-     (println "num " num " triangle num " triangle-num " divisors " divisors " divisor-count " divisor-count) 
-     (if (= divisor-count 500) num
+     (if (= divisor-count 50) num
        (recur (+ num 1)))
    )
   )
--- project-euler/src/project_euler/util.clj
+++ project-euler/src/project_euler/util.clj
@@ -4,7 +4,7 @@
   [n]
   (loop [num n factor 2 factors '()]
     (cond (> factor num) factors  
-          (= (mod num factor) 0) (recur (quot num factor) 2 (conj factors factor)) 
+          (= (mod num factor) 0) (recur (quot num factor) 2 (cons factor factors)) 
           :else (recur num (+ factor 1) factors))
   )
   )
@@ -12,8 +12,8 @@
 (defn get-all-factors
   [n]
   (loop [num n factor 1 factors '()]
-    (cond (> factor num) factors  
-          (= 0 (mod num factor)) (recur num (+ factor 1) (conj factors factor)) 
+    (cond (> factor (quot num 2)) (cons n factors)  
+          (= 0 (mod num factor)) (recur num (+ factor 1) (cons factor factors)) 
           :else (recur num (+ factor 1) factors))
   )
   )
