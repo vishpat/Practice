d9738add73803558f1667d0f5d76f4f61d17ef62
vpati011 <vishal.patil@nuagenetworks.net>
2016-10-02 15:47:30 -0400

    Checkpoint

--- project-euler/src/project_euler/problem12.clj
+++ project-euler/src/project_euler/problem12.clj
@@ -5,8 +5,7 @@
   []
   (loop [triangle-num 1 index 1]
    (let [divisors (util/get-all-factors triangle-num) divisor-count (count divisors)]
-     (println "index " index " triangle-num " triangle-num)
-     (if (>= divisor-count 5) num
+     (if (>= divisor-count 500) triangle-num 
        (recur (+ triangle-num (+ index 1)) (+ index 1)))
    )
   )
