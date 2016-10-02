975b956dfada18018875d55d80150037df8dacc3
vpati011 <vishal.patil@nuagenetworks.net>
2016-10-02 13:11:23 -0400

    Checkpoint

--- project-euler/src/project_euler/core.clj
+++ project-euler/src/project_euler/core.clj
@@ -1,2 +1,3 @@
 (ns project-euler.core
-  (require [project-euler.problem12 :as problem12]))
+  (require [project-euler.problem12 :as problem12]
+           [project-euler.util :as util]))
--- project-euler/src/project_euler/util.clj
+++ project-euler/src/project_euler/util.clj
@@ -8,3 +8,12 @@
           :else (recur num (+ factor 1) factors))
   )
   )
+
+(defn get-all-factors
+  [n]
+  (loop [num n factor 1 factors '()]
+    (cond (> factor num) factors  
+          (= 0 (mod num factor)) (recur num (+ factor 1) (conj factors factor)) 
+          :else (recur num (+ factor 1) factors))
+  )
+  )
