f1da2d795d017c281319bd79573f6534958c906a
vpati011 <vishal.patil@nuagenetworks.net>
2016-10-02 16:42:29 -0400

    Finished problem 12

--- project-euler/src/project_euler/problem12.clj
+++ project-euler/src/project_euler/problem12.clj
@@ -3,15 +3,22 @@
 
 (defn divisor-cnt
   [n]
-  (
+  (let [max-idx (Math/sqrt n)]
+     (loop [cnt 0 idx 1]
+      (cond (= n 1) 1
+            (> idx (+ max-idx)) cnt
+            (= 0 (mod n idx)) (recur (+ cnt 2) (+ idx 1))
+            :else (recur cnt (+ idx 1)))
+       )
    )
   )
+  
 
 (defn solve
   []
   (loop [triangle-num 1 index 1]
-   (let [divisors (util/get-all-factors triangle-num) divisor-count (count divisors)]
-     (if (>= divisor-count 500) triangle-num 
+   (let [cnt (divisor-cnt triangle-num)]
+     (if (>= cnt 500) triangle-num 
        (recur (+ triangle-num (+ index 1)) (+ index 1)))
    )
   )
