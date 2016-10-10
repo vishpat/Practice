fa2d1a7e9ed9262361b550fd32f51af4aa384f6a
vpati011 <vishal.patil@nuagenetworks.net>
2016-10-10 13:41:31 -0400

    Solved problem 21

--- project-euler/src/project_euler/problem21.clj
+++ project-euler/src/project_euler/problem21.clj
@@ -1,20 +1,19 @@
 (ns project-euler.problem21
   (require [project-euler.util :as util]))
 
-(defn factor-sum
-  [num]
-  (reduce + (rest (util/get-all-factors num)))
+(defn d-n
+  [n]
+  (reduce + (rest (util/get-all-factors n)))
   )
 
 (defn solve
   []
-  (loop [num 0 sum 0]
-    (println num " " sum)
-    (if (= num 10000) sum 
-      (let [num-sum (factor-sum num) sum-num-sum (factor-sum num-sum)]
-        (if (= num-sum sum-num-sum) 
-          (recur (inc num) (+ num-sum sum))
-          (recur (inc num) sum))
+  (loop [n 0 sum 0]
+    (if (= n 10000) sum 
+      (let [a (d-n n) b (d-n a)]
+        (if (and (not= a n) (= n b)) 
+          (recur (inc n) (+ n sum))
+          (recur (inc n) sum))
        )
       )
     )
