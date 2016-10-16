cd58c602c778d5d5d9a1538328415253c25294d9
vpati011 <vishal.patil@nuagenetworks.net>
2016-10-15 22:53:04 -0400

    Checkpoint

--- project-euler/src/project_euler/problem31.clj
+++ project-euler/src/project_euler/problem31.clj
@@ -1,13 +1,13 @@
 (ns project-euler.problem31)
 
 (def coins '(1 2 5 10 20 50 100 200))
-(def combinations (atom {}))
+(def combinations (atom {0 0}))
 
 (defn init-combinations
   []
   (loop [amount 1]
     (when (<= amount 200)
-          (swap! combinations assoc amount (reduce + (map #(if (= 0 (mod amount %)) (quot amount %) 0) coins))) 
+          (swap! combinations assoc amount (reduce + (map #(if (= 0 (mod amount %)) 1 0) coins))) 
           (recur (inc amount)))
    )
 )
@@ -17,17 +17,17 @@
   (do
     (init-combinations)
     (println @combinations)
-;    (loop [amount 0]
-;      (if (= amount 201) (get @combinations 200)
-;        (do
-;         (map #(let [amount-combinations (get @combinations amount) 
-;                     coin-combination (get @combinations (- amount %))] 
-;                     (if (= % amount) (assoc @combinations amount (inc amount-combinations)) 
-;                         ((when (some? coin-combination) 
-;                          (assoc @combinations amount (+ amount-combinations coin-combinations)))))) 
-;              coins) 
-;         (recur (inc amount)) 
-;          )
-;     )
+    (loop [amount 0]
+      (if (= amount 201) (get @combinations 200)
+        (do
+          (let [amount-combinations (get @combinations amount)]
+            (swap! combinations assoc amount 
+                (+ amount-combinations 
+                (reduce + (map #(let [value (get @combinations (- amount %))] (if (nil? value) 0 value)) coins))))
+          )
+          (println amount " " (get @combinations amount))
+          (recur (inc amount)))
+        )
+      )
     )
   )
