1f1bdda0044e5d7f2b50b233a13b2427d6ffeb8c
vpati011 <vishal.patil@nuagenetworks.net>
2016-10-16 01:16:16 -0400

    Checkpoint

--- project-euler/src/project_euler/problem31.clj
+++ project-euler/src/project_euler/problem31.clj
@@ -2,25 +2,18 @@
 
 (def coins '(1 2 5 10 20 50 100 200))
 (def combinations (atom {0 0}))
-
-(defn init-combinations
-  []
-  (loop [amount 1]
-    (when (<= amount 200)
-          (swap! combinations assoc amount (reduce + (map #(if (= 0 (mod amount %)) 1 0) coins))) 
-          (recur (inc amount)))
-   )
-)
+(def final-amount 4) 
 
 (defn solve
   []
   (do
-    (init-combinations)
+    (doseq [coin coins]
+      (swap! combinations assoc coin 1))
     (println @combinations)
     (loop [amount 0]
-      (if (= amount 201) (get @combinations 200)
+      (if (= amount (inc final-amount)) (get @combinations final-amount)
         (do
-          (let [amount-combinations (get @combinations amount)]
+          (let [x (get @combinations amount) amount-combinations (if x x 0)]
             (swap! combinations assoc amount 
                 (+ amount-combinations 
                 (reduce + (map #(let [value (get @combinations (- amount %))] (if (nil? value) 0 value)) coins))))
