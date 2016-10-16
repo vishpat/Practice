5a6e7061bbf378b7f50326afd95a25f4a546db4d
vpati011 <vishal.patil@nuagenetworks.net>
2016-10-15 21:30:48 -0400

    Checkpoint

--- project-euler/src/project_euler/core.clj
+++ project-euler/src/project_euler/core.clj
@@ -1,3 +1,3 @@
 (ns project-euler.core
-  (require [project-euler.problem21 :as problem21]
+  (require [project-euler.problem31 :as problem31]
            [project-euler.util :as util]))
--- project-euler/src/project_euler/problem31.clj
+++ project-euler/src/project_euler/problem31.clj
@@ -1,20 +1,40 @@
 (ns project-euler.problem31)
 
-(def coins '(1 2 5 10 20 50 100 200)
-(def combinations (@atom {}))
+(def coins '(1 2 5 10 20 50 100 200))
+(def combinations (atom {}))
+
+(defn init-combinations
+  []
+  (loop [amount 1]
+    (when (<= amount 200) 
+      (do 
+        (map #(let [amount-combinations (get @combinations amount)
+                      coin-count (quot amount %)] 
+                  (println % amount coin-count)
+                  (when (= (mod amount %) 0) 
+                    (assoc @combinations (+ amount-combinations coin-count)))) 
+               coins)
+          (recur (inc amount)))
+   )
+  )
+)
 
 (defn solve
   []
-  (loop [amount 0]
-    (if (= amount 201) (get @combinations 200)
-      (do
-       (map #(let [amount-combinations (get @combinations amount) 
-                   coin-combination (get @combinations (- amount %))] 
-                   (if (= % amount) (assoc @combinations amount (inc amount-combinations)) 
-                       ((when (some? coin-combination) 
-                        (assoc @combinations amount (+ amount-combinations coin-combinations)))))) 
-            coins) 
-       (recur (inc amount)) 
-        )
-   )
+  (do
+    (init-combinations)
+    (println @combinations)
+;    (loop [amount 0]
+;      (if (= amount 201) (get @combinations 200)
+;        (do
+;         (map #(let [amount-combinations (get @combinations amount) 
+;                     coin-combination (get @combinations (- amount %))] 
+;                     (if (= % amount) (assoc @combinations amount (inc amount-combinations)) 
+;                         ((when (some? coin-combination) 
+;                          (assoc @combinations amount (+ amount-combinations coin-combinations)))))) 
+;              coins) 
+;         (recur (inc amount)) 
+;          )
+;     )
+    )
   )
