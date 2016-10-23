837343fdc74cef44c3ca2b761b61c4ae2d38f869
vpati011 <vishal.patil@nuagenetworks.net>
2016-10-23 19:24:19 -0400

    Checkpoint

--- project-euler/src/project_euler/problem22.clj
+++ project-euler/src/project_euler/problem22.clj
@@ -1,11 +1,12 @@
 (ns project-euler.problem22
- (:require [clojure.java.io :as io]))
+ (:require [clojure.java.io :as io])
+ (:require [clojure.string :as cstr]))
 
 (defn load-data
   [data-file]
   (with-open [rdr (io/reader data-file)]
     (doseq [line (line-seq rdr)]
-      (println (clojure.string/split line #","))
+      (println (map #(cstr/replace % "\"" "" ) (sort (cstr/split line #","))))
       )      
     )
   )
