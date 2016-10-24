aeec086dda307da6d187034fc951bd1cfeade0f9
vpati011 <vishal.patil@nuagenetworks.net>
2016-10-23 23:13:52 -0400

    Project 22 solved

--- project-euler/src/project_euler/problem22.clj
+++ project-euler/src/project_euler/problem22.clj
@@ -7,24 +7,19 @@
   (reduce + (map #(inc (- (int %) (int \A))) n))
   )
 
-
-(defn load-data
+(defn load-and-solve
   [data-file]
   (with-open [rdr (io/reader data-file)]
     (doseq [line (line-seq rdr)]
       (let [scores (map name-score (map #(cstr/replace % "\"" "" ) 
-                                    (sort (cstr/split line #","))))
-            max-names (count scores)]
-          (loop [total-score 0 index 0] 
-            (if (>= index max-names) (println total-score)
-              (recur (+ total-score (* (inc index) (nth scores index))) (inc index)))
-            )
-        )
+                                    (sort (cstr/split line #","))))]
+        (println (reduce + (map-indexed #(* (inc %1) %2) scores)))
+       )
       )      
     )
   )
 
 (defn solve 
   [data-file]
-  (load-data data-file)
+  (load-and-solve data-file)
   )
