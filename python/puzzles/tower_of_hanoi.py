fa7616e1a877e33bde67047cdd32a628c29db126
vishpat <vishpat@gmail.com>
2017-10-28 11:06:29 -0400

    Added disk object

--- python/puzzles/tower_of_hanoi.py
+++ python/puzzles/tower_of_hanoi.py
@@ -1,4 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/python3.5
+
+import functools
+import random
+
+
+@functools.total_ordering
+class disk(object):
+
+    def __init__(self, weight):
+        self._w = weight
+
+    def __repr__(self):
+        return "Disk weight %d" % self._w
+
+    def __str__(self):
+        return self.__repr__(self)
+
+    def __eq__(self, obj):
+        assert isinstance(obj, disk)
+        return self._w == obj._w
+
+    def __lt__(self, obj):
+        assert isinstance(obj, disk)
+        return self._w < obj._w
 
 
 def tower_of_hanoi(n):
@@ -7,7 +31,9 @@ def tower_of_hanoi(n):
     tower2 = []
 
     for i in reversed(range(1, n + 1)):
-        tower0.append(i)
+        tower0.append(disk(random.randint(0, 100)))
+
+    tower0.sort(reverse=True)
 
     for i in reversed(range(1, n + 1)):
         while len(tower0) > 0:
