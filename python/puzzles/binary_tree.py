c09a4688db3cc189effc11979fc81d97f8f98739
Vishal Patil <vishpat@gmail.com>
2017-11-04 08:27:20 -0400

    Added LCA to binary tree

--- python/puzzles/binary_tree.py
+++ python/puzzles/binary_tree.py
@@ -40,14 +40,54 @@ class node(object):
         right_height = 0 if self._right is None else 1 + self._right.height()
         return left_height == right_height
 
+    # Least Common Ancestor
+    def lca(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        p_subtree = self.get_subtree(root, p)
+        q_subtree = self.get_subtree(root, q)
+
+        i = 0
+        while (i < len(p_subtree) and i < len(q_subtree)
+               and p_subtree[i] == q_subtree[i]):
+            i += 1
+
+        return p_subtree[i - 1]
+
+    def get_subtree(self, root, p):
+
+        if root._value == p:
+            return []
+
+        if root._left is None and root._right is None:
+            return None
+
+        if root._left:
+            subtree = self.get_subtree(root._left, p)
+            if subtree is not None:
+                subtree.insert(0, root._value)
+                return subtree
+
+        if root._right:
+            subtree = self.get_subtree(root._right, p)
+            if subtree is not None:
+                subtree.insert(0, root._value)
+                return subtree
+
+        return None
+
 
 def print_func(x):
     print(x)
 
 
 if __name__ == "__main__":
-    root = node(8)
-    for e in [5, 6, 3, 10, 12]:
+    root = node(10)
+    for e in [4, 20, 2, 6, 16, 22, 12, 18]:
         root.add_child(e)
 
-    print(root.is_balanced())
+    print(root.lca(root, 12, 6))
