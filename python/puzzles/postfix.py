bf26038d6585e5e7102c9d0db6ef4914708dccb8
Vishal Patil <vishpat@gmail.com>
2020-02-15 11:29:25 -0500

    Create postfix.py

---
+++ python/puzzles/postfix.py
@@ -0,0 +1,137 @@
+from enum import Enum
+
+
+class TokenType(Enum):
+    NUM = 1
+    ADD = 2
+    SUB = 3
+    MUL = 4
+    DIV = 5
+    LBRACE = 6
+    RBRACE = 7
+    INVALID = 8
+
+    @staticmethod
+    def type(x):
+        if str(x).isnumeric():
+            return TokenType.NUM
+
+        if x == '+':
+            return TokenType.ADD
+
+        if x == '-':
+            return TokenType.SUB
+
+        if x == '*':
+            return TokenType.MUL
+
+        if x == '/':
+            return TokenType.DIV
+
+        if x == '(':
+            return TokenType.LBRACE
+
+        if x == ')':
+            return TokenType.RBRACE
+
+        return TokenType.INVALID
+
+    @staticmethod
+    def isoperater(x):
+        return True if x == '+' or x == '-' or x == '*' or x == '/' else False
+
+    @staticmethod
+    def isoperator_add_sub(x):
+        return True if x == '+' or x == '-' else False
+
+    @staticmethod
+    def isoperator_mul_div(x):
+        return True if x == '*' or x == '/' else False
+
+
+class Token:
+
+    def __init__(self, val):
+        self.val = str(val)
+        self.type: TokenType = TokenType.type(self.val)
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.val == other
+
+        if isinstance(other, Token):
+            return self.val == other.val
+
+        return False
+
+    def __gt__(self, other):
+        if self.type == TokenType.NUM and other.type == TokenType.NUM:
+            return True if self.val > other.val else False
+
+        if TokenType.isoperater(self.val) and TokenType.isoperater(other.val):
+            return True if TokenType.isoperator_mul_div(self.val) and \
+                           TokenType.isoperator_add_sub(other.val) else False
+
+    def __str__(self):
+        return str(self.val)
+
+
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        self.stack.append(x)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def is_empty(self):
+        return False if self.stack else True
+
+    def peek(self):
+        return self.stack[-1] if not self.is_empty() else None
+
+    def __str__(self):
+        return str(self.stack)
+
+
+def infix2postfix(infix: str):
+    stack = Stack()
+    post_fix: str = ''
+
+    for ch in infix:
+        token = Token(ch)
+
+        if token.type == TokenType.NUM:
+            post_fix += token.val
+
+        if token == '(':
+            stack.push(token)
+
+        if token == ')':
+            while stack.peek() != '(':
+                post_fix += stack.pop().val
+            stack.pop()
+
+        if TokenType.isoperater(token):
+
+            if not TokenType.isoperater(stack.peek()):
+                stack.push(token)
+                continue
+
+            if token > stack.peek():
+                stack.push(token)
+            else:
+                post_fix += stack.pop().val
+                stack.push(token)
+
+    while not stack.is_empty():
+        post_fix += stack.pop().val
+
+    return post_fix
+
+
+if __name__ == "__main__":
+    print(infix2postfix("(1 + 2*3 - 4*5)"))
