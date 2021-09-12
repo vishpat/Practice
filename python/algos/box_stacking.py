5aab4b5f4295844dcc5f4c58c3a9e6988c2e2767
Vishal Patil <vishpat@gmail.com>
2021-09-12 11:50:32 -0400

    Update box_stacking.py

--- python/algos/box_stacking.py
+++ python/algos/box_stacking.py
@@ -16,23 +16,15 @@ class Box:
 
 def max_height(boxes):
     boxes = sorted(boxes, key=lambda box: (box.width, box.length))
-    total_max_height = 0
-    max_height = [box.height for box in boxes]
+    max_stack_height = [box.height for box in boxes]
     for i in range(0, len(boxes)):
         for j in range(0, i):
-            if i == j:
-                continue
-            new_max_height = max_height[j] + boxes[i].height
+            if boxes[j] < boxes[i]: 
+                new_max_stack_height = max_stack_height[j] + boxes[i].height
+                if new_max_stack_height > max_stack_height[i]:
+                    max_stack_height[i] = new_max_stack_height
 
-            if boxes[j] < boxes[i] and new_max_height > max_height[i]:
-                max_height[i] = new_max_height
-                total_max_height = (
-                    max_height[i]
-                    if max_height[i] > total_max_height
-                    else total_max_height
-                )
-
-    return total_max_height
+    return max(max_stack_height)
 
 
 if __name__ == "__main__":
